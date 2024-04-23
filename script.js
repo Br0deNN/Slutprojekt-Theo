94348fb93d7f8b10cfb293e5f2afab48aead42c9
Br0deNN <theo.broden@gmail.com>
2024-04-23 21:59:24 +0200

    typ klar

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById("loginButton").addEventListener("click", function(event) {
+    var password = document.getElementById("psw").value;
+    var minLength = 8; // Minsta antal tecken för lösenordet
+
+    if (password.length < minLength) {
+        // Om lösenordet är för kort, förhindra formuläret från att skickas och visa felmeddelande
+        event.preventDefault();
+        document.getElementById("passwordError").textContent = "Lösenordet måste vara minst " + minLength + " tecken långt.";
+        document.getElementById("psw").classList.remove("valid-password");
+        document.getElementById("psw").classList.add("invalid-password");
+    } else {
+        // Om lösenordet är tillräckligt långt, ta bort eventuella tidigare felmeddelanden och ändra border-färgen
+        document.getElementById("passwordError").textContent = "";
+        document.getElementById("psw").classList.remove("invalid-password");
+        document.getElementById("psw").classList.add("valid-password");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    
+    radioButtons.forEach(function(radioButton) {
+        radioButton.addEventListener("click", function() {
+            if (this.checked) {
+                radioButtons.forEach(function(rb) {
+                    if (rb !== radioButton) {
+                        rb.checked = false;
+                    }
+                });
+            }
+        });
+    });
+});
