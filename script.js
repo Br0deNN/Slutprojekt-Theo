a1ea868ad5a14c5ea34ac0bf1f92c880379f7a10
Br0deNN <theo.broden@gmail.com>
2024-04-29 21:15:40 +0200

    KLAR

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-
+// Lyssna efter klickhändelser på knappen med id "loginButton"
 document.getElementById("loginButton").addEventListener("click", function(event) {
+     // Hämtar lösenordet från input-elementet med id "psw"
     var password = document.getElementById("psw").value;
     var minLength = 8; // Minsta antal tecken för lösenordet
 
+      // Om lösenordet är kortare än minLength
     if (password.length < minLength) {
         // Om lösenordet är för kort, förhindra formuläret från att skickas och visa felmeddelande
         event.preventDefault();
